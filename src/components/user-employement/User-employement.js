dd6334db8083e8c09a475207df6a7cda4f0d72d5
juan <juan@192.168.0.4>
2023-09-07 17:45:05 -0500

    checkpoint

--- src/components/user-employement/User-employement.js
+++ src/components/user-employement/User-employement.js
@@ -2,13 +2,44 @@
 import './User-employement.css';
 
 function UserEmployement({ employement }) {
+
+
+    const _buildProjectRoleClient = (project) => {
+        if (project.clien) {
+            return (
+                <div className='project-role-client'>
+                    <p>{`• ${project.role} || ${project.client}`}</p>
+                </div>
+            );
+        }
+        return (
+            <div className='project-role-client'>
+                <p>{`• ${project.role}`}</p>
+            </div>
+        );
+    }
+
+    const _buildProjectDescription = (project) => {
+        return (
+            <div className='project-descriptions'>
+                {project.descriptions.map((description, index4) => (
+                    <p>{`- ${description}`}</p>
+                ))}
+            </div>
+
+        )
+    }
+
     return (
         <>
             {employement.map((job, index) => (
+
                 <div className='employment-block' key={index}>
+
                     <div className='company-title-location'>
                         <h4>{job.company_name}</h4>, <p>{job.location}</p>
                     </div>
+
                     {job.positions.map((position, index2) => (
                         <>
                             <div className='company-position' key={index2}>
@@ -16,18 +47,13 @@ function UserEmployement({ employement }) {
                                 <p>{position.date}</p>
                             </div>
                             <div className='company-projects'>
-
                                 {position.projects.map((project, index3) => (
                                     <div className='project' key={index3}>
-                                        <div className='project-role-client'>
-                                            <p >{project.role}</p>|<p>{project.client}</p>
 
-                                        </div>
-                                        {project.descriptions.map((description, index4) => (
-                                            <div className='project-descriptions'>
-                                                <p>{description}</p>
-                                            </div>
-                                        ))}
+                                        {_buildProjectRoleClient(project)}
+
+                                        {_buildProjectDescription(project)}
+
                                     </div>
                                 ))}
 
